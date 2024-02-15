68819905bf389114adefa94ec00298a94f37fbc0
leejh0469 <leejh0469@naver.com>
2024-02-16 06:07:50 +0900

    Feat - 카드 추가, 수정

--- Assets/ScriptableObjects/Card/Scripts/Card_Attack.cs
+++ Assets/ScriptableObjects/Card/Scripts/Card_Attack.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Card_Attack_", menuName = "Card/AttackCard")]
 public class Card_Attack : Card_Base
 {
-    public int attackValue;
     public override Card CreateCard()
     {
         return new AttackCard(this);
